34f01a1f96a5c8a50e53d0d17a6cabe7b3d67cd1
Johan Valero <valero_johan@hotmail.fr>
2022-03-25 09:51:06 +0100

    add files

--- Tiers_Monde/event/urls.py
+++ Tiers_Monde/event/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:event_id>/detail/', views.detail, name='detail'),
     path('informations/', views.informations, name='informations'),
+    path('register/', views.register, name='register'),
+    path('registered/', views.registered, name='registered'),
+    path('welcome/', views.welcome, name='welcome'),
+    path('login/', views.my_login, name='login'),
+    path('logout/', views.my_logout, name='logout'),
+    path('password/', views.new_password, name='password'),
 ]
--- Tiers_Monde/event/views.py
+++ Tiers_Monde/event/views.py
@@ -1,6 +1,8 @@
 from multiprocessing import context
 from django.shortcuts import render, get_object_or_404
 from . models import Events
+from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -14,3 +16,42 @@ def detail(request, event_id):
 
 def informations(request):
     return render(request, 'event/informations.html')
+
+def my_login(request):
+    return render(request, 'event/login.html')
+
+def my_logout(request):
+    logout(request)
+    return render(request, 'event/logout.html')
+
+def new_password(newpwd):
+    newpwd = User.objects.get(username ='new_password')
+    newpwd.set_password('new_password')
+    newpwd.save()
+
+def register(request):
+    return render(request, 'event/register.html')
+
+def registered(request):
+    name = request.POST['user_name']
+    firstname = request.POST['user_firstname']
+    pwd = request.POST['user_pwd']
+    email = request.POST['user_email']
+    username = firstname[0].lower() + "." + name.lower()    
+    user = User.objects.create_user(username, email, pwd)
+    user.last_name = name
+    user.first_name = firstname
+    user.save()
+    context = {'user':user}
+    return render(request, 'event/registered.html', context)
+
+def welcome(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    context = {'user':user}
+    if user is not None :
+        login(request, user)
+        return render(request, 'event/welcome.html', context)
+    else:
+        return render(request, 'event/error_log.html')
